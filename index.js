515816b3d73f81a98c1b6c5ddb6fa0b038fb19bb
Brandon <bjkrynicki@gmail.com>
2021-07-18 10:28:13 -0500

    commit tree f6ed02d6a902d4540d6df11828a0bd564cb554bc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {}
+function createEmployeeRecords() {}
+function createTimeInEvent() {}
+function createTimeOutEvent() {}
+function hoursWorkedOnDate() {}
+function wagesEarnedOnDate() {}
+function allWagesFor() {}
+function findEmployeeByFirstName() {}
+function calculatePayroll() {}
